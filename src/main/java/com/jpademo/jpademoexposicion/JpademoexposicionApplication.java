23ba04c0918cb52abd03fc2e40e94f50b174bf1d
Sebastian Bejarano <sebastian_bejarano@epam.com>
2022-09-07 22:13:38 -0500

    Initialization of repo

---
+++ src/main/java/com/jpademo/jpademoexposicion/JpademoexposicionApplication.java
@@ -0,0 +1,13 @@
+package com.jpademo.jpademoexposicion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpademoexposicionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpademoexposicionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpademo/jpademoexposicion/JpademoexposicionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpademo.jpademoexposicion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpademoexposicionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
